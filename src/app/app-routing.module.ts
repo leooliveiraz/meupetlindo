c4b600fb904b6f04c85045e0e738079255f9dcdb
Leonardo <leopdor@gmail.com>
2022-06-27 10:09:33 -0300

    Created About Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
 import { MainComponent } from './pages/main/main.component';
 
 const routes: Routes = [
+  { path: 'about', component: AboutComponent },
   { path: '', component: MainComponent },
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { ContentComponent } from './content/content.component';
 import { MainComponent } from './pages/main/main.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AboutComponent } from './pages/about/about.component';
 
 
 @NgModule({
@@ -26,6 +27,7 @@ import { AppRoutingModule } from './app-routing.module';
     SidebarComponent,
     ContentComponent,
     MainComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
