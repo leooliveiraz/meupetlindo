af30cf01bf8fbe9e386082d35147b27d48f2a0b3
Leonardo <leopdor@gmail.com>
2022-07-02 17:54:19 -0300

    Drugs Page

--- src/app/pages/drugs/drugs.component.ts
+++ src/app/pages/drugs/drugs.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AnimalService } from 'src/app/services/animal.service';
+import { MedicarService } from 'src/app/services/medicar.service';
+import { ToastService } from 'src/app/services/toast.service';
+import { environment } from 'src/environments/environment';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-drugs',
@@ -7,9 +13,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DrugsComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private animalService: AnimalService,
+    private medicarService: MedicarService,
+    private toastService: ToastService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+  ) {
   }
 
+  idAnimal: any = null;
+  tipoMedicacao: any = null;
+  nome = null;
+  dataMedicamento = '';
+  dataProxima = '';
+  urlImagem = `${environment.API_URL}arquivo/`;
+
+  listaAnimais: any = [];
+  salvando = false;
+
+  ngOnInit() {
+    const animal = this.activatedRoute.snapshot.queryParamMap.get('animal');
+    if (animal) {
+      this.idAnimal = parseInt(animal);
+    }
+    this.carregarAnimais();
+  }
+
+  cadastrar(f: any) {
+    if (f.form.status === 'INVALID') {
+      Swal.fire('', 'Por favor, escolha o bichinho, e informe a data e a medicação.', 'warning');
+      return;
+    }
+    this.salvando = true;
+    const medicacao: any = f.form.value;
+    this.medicarService.salvar(medicacao).subscribe(res => {
+      this.toastService.create('success', 'A medicação foi adicionada nas informações do seu bichinho!');
+      this.router.navigateByUrl(`/animal/${medicacao.idAnimal}`);
+    }, erro => {
+      this.salvando = false;
+      this.toastService.create('error', 'Não foi possível salvar essa medicação! Tente novamente mais tarde');
+    });
+  }
+
+  carregarAnimais() {
+    this.animalService.listar().subscribe(res => {
+      this.listaAnimais = res;
+    }, erro => {
+    });
+  }
 }
--- todo.md
+++ todo.md
@@ -5,9 +5,9 @@ Create a Sidebar {cm:2022-06-24}
 Create My Animals Page {cm:2022-07-01}
 Create Add Animal Page {cm:2022-06-30}
 Create Exams Page {cm:2022-07-01}
-Create Animal Page
+Create Animal Page {start:2022-06-28T23:31:41}
 Create Weight Record Page {cm:2022-07-01}
-Create Drugs Page
+Create Drugs Page {cm:2022-07-01}
 Login functionality {cm:2022-06-29}
 Create main page {cm:2022-06-27}
 Create Share Animals funcionality
