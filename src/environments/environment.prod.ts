aec498564d9843bc44850eed1cf2a8459202b863
Leonardo <leopdor@gmail.com>
2022-07-27 18:41:45 -0300

    Fixed env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   GOOGLE_OAUTH2: '901875702030-dbvtct82nqvhb393uj118mf8g8g8v66s.apps.googleusercontent.com',
   OAUTH2_REDIRECT: 'https://leooliveiraz.github.io/meupetlindo',
   API_URL: 'https://meudoglindo.herokuapp.com/',
-  APP_URL: 'https://leooliveiraz.github.io/meupetlindo/',,
+  APP_URL: 'https://leooliveiraz.github.io/meupetlindo/',
   VAPID_PUBLIC_KEY: "BJIEHJOP5D2qNYFVA23FXzd6yLGvG3mRh7lr5mq8HjNvlucsIDo_AeIP-xc-IfBLiFHZkuEWMehY6XapWJsaG8k"
 };
