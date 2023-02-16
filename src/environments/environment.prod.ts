eb4f72a60466894696cec7b86e96079c3a69ea2d
Leonardo <leopdor@gmail.com>
2023-02-16 12:47:33 -0300

    Alterado URL API

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   GOOGLE_OAUTH2: '901875702030-dbvtct82nqvhb393uj118mf8g8g8v66s.apps.googleusercontent.com',
   OAUTH2_REDIRECT: 'https://leooliveiraz.github.io/meupetlindo',
-  API_URL: 'https://meudoglindo.herokuapp.com/',
+  API_URL: 'https://mpl.tunnelto.dev/api/',
   APP_URL: 'https://leooliveiraz.github.io/meupetlindo/',
   VAPID_PUBLIC_KEY: "BJIEHJOP5D2qNYFVA23FXzd6yLGvG3mRh7lr5mq8HjNvlucsIDo_AeIP-xc-IfBLiFHZkuEWMehY6XapWJsaG8k"
 };
