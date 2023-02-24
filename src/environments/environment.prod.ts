7824ff66bb5f73b095088d1b0ce5efbeafe7e184
Leonardo <leopdor@gmail.com>
2023-02-24 08:41:09 -0300

    Deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   GOOGLE_OAUTH2: '901875702030-dbvtct82nqvhb393uj118mf8g8g8v66s.apps.googleusercontent.com',
   OAUTH2_REDIRECT: 'https://leooliveiraz.github.io/meupetlindo',
   API_URL: 'https://mpl.tunnelto.dev/api/',
-  APP_URL: 'https://leooliveiraz.github.io/meupetlindo/',
+  APP_URL: 'https://www.meupetlindo.com/',
   VAPID_PUBLIC_KEY: "BJIEHJOP5D2qNYFVA23FXzd6yLGvG3mRh7lr5mq8HjNvlucsIDo_AeIP-xc-IfBLiFHZkuEWMehY6XapWJsaG8k",
-  APP_BASE_HREF: '/meupetlindo/'
+  APP_BASE_HREF: '/'
 };
